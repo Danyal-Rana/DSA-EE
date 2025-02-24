7bda9e4ca2286b34cae881f5891d29cb816f845b
Danyal Rana <ranadanyalarshad@gmail.com>
2025-02-24 17:33:40 +0500

    Add push function and stack display for linked list implementation

---
+++ 2-Stack-via-LinkedList/push.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+// inserting function / push function
+void push(int data, struct Node **top)
+{
+    struct Node *newNode;
+    newNode = malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (top!=NULL)
+    {
+        newNode->next = *top;
+        
+    }
+    *top = newNode;
+}
+
+void showStack (struct Node *temp)
+{
+    while (temp!=NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main () {
+
+    struct Node *top = NULL;
+
+    push(2, &top);
+    push(3, &top);
+    push(4, &top);
+    push(5, &top);
+
+    struct Node *temp = top;
+    showStack(temp);
+    return 0;
+}
