5282f94733761a32f42d326dc6a6574778a568ba
Danyal Rana <ranadanyalarshad@gmail.com>
2025-02-24 18:53:41 +0500

    Add isEmpty function and main program for linked list stack implementation

---
+++ 2-Stack-via-LinkedList/isEmpty.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+// inserting function / push function
+void push(int data, struct Node **top)
+{
+    struct Node *newNode;
+    newNode = malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (*top!=NULL)
+    {
+        newNode->next = *top;
+        *top = newNode;
+    }
+    else
+    {
+        *top = newNode;
+    }
+}
+
+// whether the stack is empty or not
+bool isEmpty(struct Node **temp)
+{
+    if (*temp==NULL)
+    {
+        return true;
+    }
+    return false;
+}
+
+void showStack (struct Node *temp)
+{
+    while (temp!=NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main () {
+
+    struct Node *top = NULL;
+
+    bool a = isEmpty(&top);
+    if (a)
+    {
+        printf("Stack is Empty\n");
+    }
+    else
+    {
+        printf("Stack is Not empty\n");
+    }
+
+    push(2, &top);
+    push(3, &top);
+    push(4, &top);
+    push(5, &top);
+
+    struct Node *temp = top;
+    showStack(temp);
+    printf("\n");
+
+    a = isEmpty(&top);
+    if (a)
+    {
+        printf("Stack is Empty\n");
+    }
+    else
+    {
+        printf("Stack is Not empty\n");
+    }
+
+    return 0;
+}
